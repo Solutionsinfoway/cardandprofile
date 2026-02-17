09c9d9c023ecec3c631c14b9fb110b3386af9357
Solutionsinfoway <operations@solutionsinfoway.com>
2026-02-17 13:46:21 +0530

    connection string updation

--- db.php
+++ db.php
@@ -1,9 +1,13 @@
 <?php
-$connectionString = getenv("SQLAZURECONNSTR_Defaultconnection");
+$server = "tcp:yourservername.database.windows.net,1433";
+$database = "Emp_Details";
+$username = "sqladmin";
+$password = "Admin@123";
 
 try {
-    $conn = new PDO("sqlsrv:$connectionString");
+    $conn = new PDO("sqlsrv:server=$server;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
