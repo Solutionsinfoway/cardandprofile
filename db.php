a136ff97456cf20032d373852f522daf2a1e439b
Solutions <100860593+Solutionsinfoway@users.noreply.github.com>
2026-02-17 19:59:20 +0530

    connectsql db.php

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $password = getenv("DB_PASS");
 try {
     $conn = new PDO("sqlsrv:server=$server;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully!";
+    echo "Connected successfully!";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
