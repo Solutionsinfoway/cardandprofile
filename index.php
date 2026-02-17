27a616bd303e8dde912371da540cf606ddc52e09
Solutions <100860593+Solutionsinfoway@users.noreply.github.com>
2026-02-17 19:57:33 +0530

    hello index.php

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php
-include 'db.php';
-
-$sql = "SELECT * FROM Employees";
-$stmt = $conn->query($sql);
-
-echo "<h2>Employee List</h2>";
-echo "<table border='2'>";
-<<<<<<< HEAD
-echo "<tr><th>ID</th><th>Name</th><th>Email</th></tr>";
-=======
-echo "<tr><th>ID</th><th>Name</th><th>Email</th><th>Company_Name</th></tr>";
->>>>>>> 3f9b98e39c5e8f5496d0d1275fad54f42f1a1d12
-
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo "<tr>";
-    echo "<td>".$row['EmpID']."</td>";
-    echo "<td>".$row['FullName']."</td>";
-    echo "<td>".$row['Email']."</td>";
-<<<<<<< HEAD
-    echo "<td>".$row[]
-=======
-    echo "<td>".$row['Company_Name']."</td>";
->>>>>>> 3f9b98e39c5e8f5496d0d1275fad54f42f1a1d12
-    echo "</tr>";
-}
-echo "</table>";
+echo "Hello";
 ?>
